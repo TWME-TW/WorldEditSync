b2f4685395669763c703a228a4284b66a464660d
TWME <65117253+TWME-TW@users.noreply.github.com>
2025-01-05 15:40:06 +0800

    0.0.4

--- src/main/java/dev/twme/worldeditsync/velocity/WorldEditSyncVelocity.java
+++ src/main/java/dev/twme/worldeditsync/velocity/WorldEditSyncVelocity.java
@@ -18,7 +18,7 @@ import java.util.concurrent.TimeUnit;
 @Plugin(
         id = "worldeditsync",
         name = "WorldEditSync",
-        version = "0.0.3",
+        version = "0.0.4",
         description = "Sync WorldEdit clipboard across servers",
         authors = {"TWME"}
 )
